2fca8610905313ed2cfe0098ca5ee7749968be4b
arshan <arshankhanifar@gmail.com>
2023-12-01 11:59:29 -0500

    changed it to their version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,29 +3,23 @@ import "@rainbow-me/rainbowkit/styles.css";
 import "@decent.xyz/the-box/index.css";
 import {
   connectorsForWallets,
-  getDefaultWallets,
   RainbowKitProvider,
 } from "@rainbow-me/rainbowkit";
 import type { AppProps } from "next/app";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { sepolia, zoraTestnet } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
+import { songcampChains } from "./index";
+import { getWagmiChain } from "@decent.xyz/box-common";
 
 const { chains, publicClient } = configureChains(
-  [sepolia, zoraTestnet],
+  songcampChains.map((chain) => getWagmiChain(chain)),
   [
     alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY! }),
     publicProvider(),
   ],
 );
-
-//const { connectors } = getDefaultWallets({
-//  appName: "RainbowKit App",
-//  projectId: "YOUR_PROJECT_ID",
-//  chains,
-//});
 
 const connectors = connectorsForWallets([
   {
--- pages/index.tsx
+++ pages/index.tsx
@@ -27,9 +27,15 @@ import { useState } from "react";
 import { songcampAbi } from "../constants/songcampAbi";
 import { getPublicClient } from "@wagmi/core";
 
-const chains = [ChainId.SEPOLIA, ChainId.ZORA_GOERLI];
+export const songcampChains = [
+  ChainId.ETHEREUM,
+  ChainId.ARBITRUM,
+  ChainId.BASE,
+  ChainId.ZORA,
+  ChainId.OPTIMISM,
+];
 
-const nftAddress: EvmAddress = "0xd643567B131777cD52841Ca1FF7663Ba890a0092";
+const nftAddress: EvmAddress = "0x3547F3cF6dad2cE64b5c308ebD964822220cF577";
 export const useNftBalance = ({
   address,
   token,
@@ -48,13 +54,32 @@ export const useNftBalance = ({
     watch: true,
   });
 };
+const merkleProof = [
+  "0xe4c348a91d98afc9be34587b630b53d599fadf00b655aea0dba9d87e39c7ca19",
+  "0x9454e9c161bae72807be02a275db91eb8e8d4bb4d8c6b6af8955477bd03f726d",
+  "0xc2f06859a23d02845f905e32c172c180a25bda23804a3f61b61dd8e9fa5ea6ac",
+  "0x38935e46368c6d84360c3cff51bf788643218112625ed7f1d7a6809cd3f937fa",
+  "0xfc92335526bab93be49c02ffa6504b6ee3bee15e48b69f6e0906ee7e3b5160b6",
+  "0xf996a9323724d10ceea077c2ffcf1c4d7fb2fd24172539a6d374748fde65be2d",
+  "0xdaf3a7b0639d4a2274cf741bdead54179a19a807209d7f74937f01739edc89cb",
+  "0x20c957373246d755764c04ed2e6a150395e8d21b552d39666c1dbed9b69822cc",
+  "0x86a8000ad0134dd657428e846b4316834a24f81d49d6b133364769c24bfe01b7",
+  "0x9e0f22fba4aa912e601b2fe26937f8ed727236413de7a3517a99eaa84bc5efcd",
+  "0x010304112c2b08212bc80aff5b9db1969a434012ed5a567492d9d59bb6882cb0",
+  "0xbeb3d4bcf7c30b99803d134f26b02ffa09095706edd5982a0decdd7c37809175",
+  "0x7f98040718bb79505a892348efc38082a046f2236b852b5e3fdfac371246dd1d",
+  "0x3e9435508e548fab3b3e149e20f120a861baf108cf69920f83537e80c5757e42",
+  "0xe489723358ffa56413007e56d0a481dfe9b4c00df9abbb4eecb7a90d1b643d4c",
+  "0x88a8494edc0ede97ada0e7a8b4d5ef3c7f481461fbae6b4ccf49a9e711e909b9",
+  "0x28ea26022aa672e0258dc935b08984f4dd08317f553034169d4ae41ca916210a",
+];
 
 const FirstBox = () => {
   const { address } = useAccount();
   const { data: balance } = useNftBalance({
     address: address!,
     token: nftAddress,
-    chainId: ChainId.ZORA_GOERLI,
+    chainId: ChainId.ZORA,
   });
 
   return (
@@ -70,17 +95,18 @@ const FirstBox = () => {
         className="rounded-lg border shadow-md bg-white px-24"
         paymentButtonText="Mint an NFT"
         actionType={ActionType.NftMint}
-        chains={chains}
+        chains={songcampChains}
         actionConfig={
           {
             contractAddress: nftAddress,
-            chainId: ChainId.ZORA_GOERLI,
+            chainId: ChainId.ZORA,
             cost: {
               isNative: true,
-              amount: parseEther("0.00001"),
+              amount: 80n,
             },
-            signature: "function safeMint(address to) payable",
-            args: [address],
+            signature:
+              "function allowListMultiMint(bytes32[] merkleProof, uint256 numberOfTokens, address to) public",
+            args: [merkleProof, 1, address],
             deliverEth: true,
           } as any
         }
@@ -91,7 +117,7 @@ const FirstBox = () => {
 };
 
 const NftInfo = () => {
-  const chainId = ChainId.ZORA_GOERLI;
+  const chainId = ChainId.ZORA;
   console.log({ chainId });
   const [nftId, setNftId] = useState<number>(2);
   const { address } = useAccount();
@@ -221,11 +247,11 @@ const SecondBox = () => {
         className="rounded-lg border shadow-md bg-white px-24"
         paymentButtonText="Write to DISC"
         actionType={ActionType.NftMint}
-        chains={chains}
+        chains={songcampChains}
         actionConfig={
           {
             contractAddress: nftAddress,
-            chainId: ChainId.ZORA_GOERLI,
+            chainId: ChainId.ZORA,
             signature:
               "function multiWriteToDiscSignature(uint256[] memory tokenIds,uint256[] memory songSelections,bytes memory signature)",
             args: [tokenIds, songSelections, signature as Hex],
@@ -235,7 +261,7 @@ const SecondBox = () => {
         disableGuard={async () => {
           try {
             const publicClient = getPublicClient({
-              chainId: ChainId.ZORA_GOERLI,
+              chainId: ChainId.ZORA,
             });
             await publicClient.simulateContract({
               address: nftAddress,
