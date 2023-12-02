1ee6817acc9f622c73e471a57bac8b5e9ac865f8
arshan <arshankhanifar@gmail.com>
2023-12-01 21:17:55 -0500

    retrieving merkle proof

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,7 @@ import { ClientRendered } from "@decent.xyz/box-ui";
 import { useState } from "react";
 import { songcampAbi } from "../constants/songcampAbi";
 import { getPublicClient } from "@wagmi/core";
+import useSWR from "swr";
 
 export const songcampChains = [
   ChainId.ETHEREUM,
@@ -54,25 +55,6 @@ export const useNftBalance = ({
     watch: true,
   });
 };
-const merkleProof = [
-  "0xe4c348a91d98afc9be34587b630b53d599fadf00b655aea0dba9d87e39c7ca19",
-  "0x9454e9c161bae72807be02a275db91eb8e8d4bb4d8c6b6af8955477bd03f726d",
-  "0xc2f06859a23d02845f905e32c172c180a25bda23804a3f61b61dd8e9fa5ea6ac",
-  "0x38935e46368c6d84360c3cff51bf788643218112625ed7f1d7a6809cd3f937fa",
-  "0xfc92335526bab93be49c02ffa6504b6ee3bee15e48b69f6e0906ee7e3b5160b6",
-  "0xf996a9323724d10ceea077c2ffcf1c4d7fb2fd24172539a6d374748fde65be2d",
-  "0xdaf3a7b0639d4a2274cf741bdead54179a19a807209d7f74937f01739edc89cb",
-  "0x20c957373246d755764c04ed2e6a150395e8d21b552d39666c1dbed9b69822cc",
-  "0x86a8000ad0134dd657428e846b4316834a24f81d49d6b133364769c24bfe01b7",
-  "0x9e0f22fba4aa912e601b2fe26937f8ed727236413de7a3517a99eaa84bc5efcd",
-  "0x010304112c2b08212bc80aff5b9db1969a434012ed5a567492d9d59bb6882cb0",
-  "0xbeb3d4bcf7c30b99803d134f26b02ffa09095706edd5982a0decdd7c37809175",
-  "0x7f98040718bb79505a892348efc38082a046f2236b852b5e3fdfac371246dd1d",
-  "0x3e9435508e548fab3b3e149e20f120a861baf108cf69920f83537e80c5757e42",
-  "0xe489723358ffa56413007e56d0a481dfe9b4c00df9abbb4eecb7a90d1b643d4c",
-  "0x88a8494edc0ede97ada0e7a8b4d5ef3c7f481461fbae6b4ccf49a9e711e909b9",
-  "0x28ea26022aa672e0258dc935b08984f4dd08317f553034169d4ae41ca916210a",
-];
 
 const FirstBox = () => {
   const { address } = useAccount();
@@ -80,6 +62,12 @@ const FirstBox = () => {
     address: address!,
     token: nftAddress,
     chainId: ChainId.ZORA,
+  });
+  const url = `https://c4-generator.vercel.app/api/generateproof?address=${address}`;
+  const { data: merkleProof } = useSWR(address ? url : null, async () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((data) => data.proofArray);
   });
 
   return (
@@ -91,27 +79,29 @@ const FirstBox = () => {
           </h1>
         </div>
       </div>
-      <TheBox
-        className="rounded-lg border shadow-md bg-white px-24"
-        paymentButtonText="Mint an NFT"
-        actionType={ActionType.NftMint}
-        chains={songcampChains}
-        actionConfig={
-          {
-            contractAddress: nftAddress,
-            chainId: ChainId.ZORA,
-            cost: {
-              isNative: true,
-              amount: 80n,
-            },
-            signature:
-              "function allowListMultiMint(bytes32[] merkleProof, uint256 numberOfTokens, address to) public",
-            args: [merkleProof, 1, address],
-            deliverEth: true,
-          } as any
-        }
-        apiKey={process.env.NEXT_PUBLIC_DECENT_API_KEY as string}
-      />
+      {merkleProof && (
+        <TheBox
+          className="rounded-lg border shadow-md bg-white px-24"
+          paymentButtonText="Mint an NFT"
+          actionType={ActionType.NftMint}
+          chains={songcampChains}
+          actionConfig={
+            {
+              contractAddress: nftAddress,
+              chainId: ChainId.ZORA,
+              cost: {
+                isNative: true,
+                amount: 80n,
+              },
+              signature:
+                "function allowListMultiMint(bytes32[] merkleProof, uint256 numberOfTokens, address to) public",
+              args: [merkleProof, 1, address],
+              deliverEth: true,
+            } as any
+          }
+          apiKey={process.env.NEXT_PUBLIC_DECENT_API_KEY as string}
+        />
+      )}
     </div>
   );
 };
